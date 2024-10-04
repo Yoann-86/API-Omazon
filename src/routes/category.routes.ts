50d520ad72d4c3ab75616820b41e8d0d8342b75a
Yoann Auroy <yoann.ar.pro@gmail.com>
2024-10-04 17:26:32 +0200

    swagger categories routes

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -8,3 +8,49 @@ router.all("/", categoryController.methodNotAllowed);
 router.all("/:id", categoryController.methodNotAllowedParams);
 
 export default router;
+
+/**
+ * @openapi
+ * /api/categories:
+ *  get:
+ *     summary: Get all categories or one using query params with id.
+ *     tags: [Categories]
+ *     description:
+ *       Return all categories if no query params are provided or one product using query params with the product id (/api/categories?id=123456).
+ *     parameters:
+ *     - in: query
+ *       name: id
+ *     schema:
+ *      type: string
+ *      required: false
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The status of the response.
+ *                   example: "success"
+ *                 data:
+ *                   $ref: '#/components/schemas/Category'
+ *       404:
+ *         description: NOT FOUND
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   description: The status of the response.
+ *                   example: "error"
+ *                 message:
+ *                   type: string
+ *                   description: The message of the response.
+ *                   example: "No categories found"
+ *
+ */
